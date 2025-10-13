b9e03a640b460abc5480c64b476bf7fb4865a143
Edilson <egarciav27@miumg.edu.gt>
2025-10-12 20:33:49 -0600

    Versión inicial del proyecto Qt

---
+++ SO_Suite/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_SO_Suite_FILE /home/edilson/Escritorio/sistemas operativos/SO_Suite/SO_Suite/build/Desktop_Qt_6_9_2-Debug/SO_Suite)
+set(__QT_DEPLOY_TARGET_SO_Suite_TYPE EXECUTABLE)
